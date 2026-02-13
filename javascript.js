9d93badf5892e1a76583a973ec9c8318506a5481
Arif <153439486+clorifdai@users.noreply.github.com>
2026-02-13 20:05:31 +0800

    Writing rock-paper-scissors logic

--- javascript.js
+++ javascript.js
@@ -18,13 +18,60 @@ function getComputerChoice() {
   } else {
     choice = "scissors";
   }
-  console.log(choice)
+  // choice = "rock";
+  return choice;
+  console.log(choice);
 }
 
 function getHumanChoice() {
   let choice = prompt('Write “rock,” “paper,” “scissors.” Enter input.');
+  // ubah ke huruf kecil
+  return choice;
   console.log(choice)  
 }
 
-computer = getComputerChoice()
-human = getHumanChoice()
+let computerScore = 0;
+let humanScore = 0;
+
+function playRound (computerChoice, humanChoice) {
+  humanChoice = humanChoice.toLowerCase()
+  console.log(computerChoice, humanChoice)
+  // Paper
+  if (computerChoice === "rock" && humanChoice === "paper") {
+    console.log("Human menang");
+    humanScore += 1;
+  } else if (computerChoice === "paper" && humanChoice === "paper") {
+    console.log("Imbang");
+    computerScore += 0;
+  } else if (computerChoice === "scissors" && humanChoice === "paper") {
+    console.log("Computer menang");
+    computerScore += 1;
+  } else /* Scissors */  if (computerChoice === "rock" && humanChoice === "scissors") {
+    console.log("Computer menang");
+    computerScore += 1;
+  } else if (computerChoice === "paper" && humanChoice === "scissors") {
+    console.log("Human menang");
+    humanScore += 1;
+  } else if (computerChoice === "scissors" && humanChoice === "scissors") {
+    console.log("Imbang");
+    computerScore += 0;
+  } else /* Rock*/ if (computerChoice === "rock" && humanChoice === "rock") {
+    console.log("imbang");
+    computerScore += 0;
+  } else if (computerChoice === "paper" && humanChoice === "rock") {
+    console.log("Computer menang");
+    computerScore += 1;
+  } else if (computerChoice === "scissors" && humanChoice === "rock") {
+    console.log("Human menang");
+    humanScore += 1;
+  } else null
+  
+  console.log("Computer score: ", computerScore);
+  console.log("Human score: ", humanScore)
+
+}
+
+
+const computer = getComputerChoice()
+const human = getHumanChoice()
+playRound(computer, human) 
